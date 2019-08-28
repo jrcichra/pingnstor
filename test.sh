b1cb7da6111032d1384eb8dbb54570d675b68b45
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-08-27 22:09:09 -0400

    workaround for host mysql

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 docker network create test-net
-docker run --name mysql --network=test-net -e MYSQL_ROOT_PASSWORD=test -d mysql:8.0.17
-docker exec mysql mysql -uroot -ptest -e "$(cat setup.sql)"
-docker run --name --network=test-net pingnstor jrcichra/pingnstor:$TRAVIS_COMMIT
+docker run --name pingnstor --network="host" jrcichra/pingnstor:${TRAVIS_COMMIT}
 sleep 120
-docker exec mysql mysql -uroot -ptest -e "select count(*) from pingnstor.pings where domain = 'google.com'"
+mysql -uroot -sse "select count(*) from pingnstor.pings where domain = 'google.com'"
 docker kill pingnstor
