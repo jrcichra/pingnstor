ec0663e8ec82386f6c841cc95a3e00158746f0cb
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-08-27 21:58:18 -0400

    Put docker test in script

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-if [ $(mysql -uroot -sse "select count(*) from pingnstor.pings where domain = 'google.com'") -gt 0 ]
-then
-exit 0
-else
-exit 1
-fi
+docker network create test-net
+docker run --name mysql --network=test-net -e MYSQL_ROOT_PASSWORD=test -d mysql:8.0.17
+docker exec mysql mysql -uroot -ptest -e "$(cat setup.sql)"
+docker run --name --network=test-net pingnstor jrcichra/pingnstor:$TRAVIS_COMMIT
+sleep 120
+docker exec mysql mysql -uroot -ptest -e "select count(*) from pingnstor.pings where domain = 'google.com'"
+docker kill pingnstor
